bf8f4629fdc4e8a15dd564a520fa370f9dd20933
DimaKozhanovsky <koganovski1989koganovski@gmail.com>
2022-08-13 23:31:02 +0300

    feat : refacoring + 2 new buttons to handle the opportunity to visit site

---
+++ welcome.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    document.querySelector('.bg-image').classList.remove('bg-image');
+      document.querySelector('.demo').remove()
+
+    // if (confirm("Press a button!")) {
+    //   document.querySelector('.bg-image').classList.remove('bg-image');
+    //   document.querySelector('.demo').remove()
+    // } else {
+      
+    // }
+    // document.getElementById("demo").innerHTML = txt;
+  }
+  function myNoFunction(){
+    window.location ="https://github.com/DimaKozhanovsky/rsschool-cv"
+  }
